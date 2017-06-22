d817da67489a496d6e94918c2734662fafea56dc
David Roncal Vidal <david.roncal@lim.upeu.edu.pe>
2017-06-22 12:22:47 -0500

    test

--- django_rv_apps/apps/believe_his_prophets_api/views/application_view.py
+++ django_rv_apps/apps/believe_his_prophets_api/views/application_view.py
@@ -59,14 +59,18 @@ class ApplicationViewSet(viewsets.ModelViewSet):
                         'content':obj_version['content'],
                         'condition':True
                     }
+                    print('obj_response 1', obj_response)
+
                 else:
                     obj_response={
                         'condition':False
                     }
+                    print('obj_response 2', obj_response)
         else:
             obj_response={
                 'condition':False
             }
-        print('obj_response', obj_response)
+            print('obj_response 3', obj_response)
+        print('obj_response 4 ', obj_response)
 
         return Response(obj_response)
