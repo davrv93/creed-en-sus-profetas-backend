90b265a1b1718dba8205306b6af13bfda46fb155
David <davrv93@gmail.com>
2018-11-17 22:40:55 -0500

    update filters

--- django_rv_apps/apps/believe_his_prophets_api/views/chapter/filters.py
+++ django_rv_apps/apps/believe_his_prophets_api/views/chapter/filters.py
@@ -1,8 +1,5 @@
 import django_filters
 
-from django_filters import rest_framework as filters
-
-
 from django_rv_apps.apps.believe_his_prophets.models.chapter import Chapter
 
 
@@ -10,4 +7,4 @@ class ChapterFilter(django_filters.FilterSet):
 
     class Meta:
         model = Chapter
-        fields = ('id', 'book')
+        fields = ('id', 'book','chapter')
