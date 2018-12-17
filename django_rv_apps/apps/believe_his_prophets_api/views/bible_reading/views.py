1b65f48f85624b0a73b4b678aa457b2e7bcf8124
Samuel David Roncal Vidal <davrv93@gmail.com>
2018-12-16 20:39:04 -0500

    update from jenkins

--- django_rv_apps/apps/believe_his_prophets_api/views/bible_reading/views.py
+++ django_rv_apps/apps/believe_his_prophets_api/views/bible_reading/views.py
@@ -1,7 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+
+
 from django_filters import rest_framework as django_filters
 from rest_framework import viewsets
 
+from django_rv_apps.apps.believe_his_prophets.models.verse import Verse
 from django_rv_apps.apps.believe_his_prophets.models.bible_read import BibleRead
+from django_rv_apps.apps.believe_his_prophets.models.chapter import Chapter
+
 from django_rv_apps.apps.believe_his_prophets_api.views.bible_reading.serializers\
     import BibleReadSerializer
 from django_rv_apps.apps.believe_his_prophets_api.views.bible_reading.filters \
@@ -11,6 +20,9 @@ from django_rv_apps.apps.believe_his_prophets_api.views.bible_reading.mixins.rea
 from django_rv_apps.apps.believe_his_prophets_api.views.bible_reading.mixins.picture \
     import PictureMixin
 
+from django_rv_apps.apps.believe_his_prophets_api.views.verse.serializers import VerseSerializer
+
+from django_rv_apps.apps.believe_his_prophets_api.views.book.serializers import BookSerializer
 
 class ReadingViewSet(viewsets.ModelViewSet, ReadingMixin, PictureMixin):
 
@@ -23,3 +35,51 @@ class ReadingViewSet(viewsets.ModelViewSet, ReadingMixin, PictureMixin):
         if self.paginator and self.request.query_params.get(self.paginator.page_query_param, None) is None:
             return None
         return super().paginate_queryset(queryset)
+
+
+class BibleReadingView(APIView):
+    #authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = ()
+
+    def get(self, request, format=None):
+
+        retorno = dict()
+
+        date = request.query_params.get('date')
+
+        code_iso = request.query_params.get('code_iso','').upper()
+
+        bible_read = BibleRead.objects.get(start_date=date)
+
+        book = bible_read.book
+
+        serializer = BookSerializer(book, context={
+            'code_iso':code_iso
+        })
+
+        retorno.update(
+            book=serializer.data
+        )
+
+        chapter = bible_read.start_chapter
+
+        retorno.update(
+            chapter=chapter
+        )
+
+        verses = Verse.objects.filter(
+            book=book,
+            language__code_iso = code_iso,
+            chapter = chapter
+        )
+
+        serializer = VerseSerializer(
+            verses, many=True
+        )
+
+        retorno.update(
+            verses=serializer.data
+        )
+
+
+        return Response(retorno)
--- django_rv_apps/apps/believe_his_prophets_api/views/book/serializers.py
+++ django_rv_apps/apps/believe_his_prophets_api/views/book/serializers.py
@@ -1,13 +1,33 @@
 from rest_framework import serializers
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.decorators import list_route
-from rest_framework.exceptions import ParseError
-from django_rv_apps.apps.believe_his_prophets.models.book import Book
+
+from django_rv_apps.apps.believe_his_prophets.models.book import Book, BookLanguage
 
 
 class BookSerializer(serializers.ModelSerializer):
 
+    name = serializers.SerializerMethodField()
+
     class Meta:
         model = Book
         fields = '__all__'
+
+    def get_name(self,obj):
+
+        code_iso = self.context.get('code_iso')
+
+        print('code_iso',code_iso)
+
+        book_language = BookLanguage.objects.get(
+            book=obj.id,
+            language__code_iso=code_iso
+        )
+
+        if book_language:
+
+            return book_language.name
+
+        else:
+
+            return ''
+
+
