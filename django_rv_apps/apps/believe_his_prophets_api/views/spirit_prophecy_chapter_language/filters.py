657d767f6ac6b3e736895d65f53c5cb018820ec5
david roncal <davrv93@gmail.com>
2021-10-01 10:46:12 -0500

    filtros

--- django_rv_apps/apps/believe_his_prophets_api/views/spirit_prophecy_chapter_language/filters.py
+++ django_rv_apps/apps/believe_his_prophets_api/views/spirit_prophecy_chapter_language/filters.py
@@ -5,6 +5,7 @@ from django_filters import rest_framework as filters
 from django_rv_apps.apps.believe_his_prophets.models.spirit_prophecy_chapter import SpiritProphecyChapter, SpiritProphecyChapterLanguage
 from django_rv_apps.apps.believe_his_prophets.models.spirit_prophecy import SpiritProphecy
 from django_rv_apps.apps.believe_his_prophets.models.language import Language
+from django.utils import timezone
 
 
 class SpiritProphecyChapterLanguageFilter(django_filters.FilterSet):
@@ -15,17 +16,19 @@ class SpiritProphecyChapterLanguageFilter(django_filters.FilterSet):
         to_field_name='code_iso'
     )
 
-    start_date = filters.DateFilter(
-        field_name='spirit_prophecy_chapter__start_date',
-        lookup_expr='lte'
-    )
-    end_date = filters.DateFilter(
-        field_name='spirit_prophecy_chapter__end_date',
-        lookup_expr='gte'
+    start_date = filters.CharFilter(method='filter_date')
 
-    )
 
+
+ 
     class Meta:
         model = SpiritProphecyChapterLanguage
-        fields = ('id', 'spirit_prophecy_chapter__start_date',
-                  'spirit_prophecy_chapter__end_date')
+        fields = ('id' ,'code_iso','start_date')
+
+
+    def filter_date(self, queryset, name, value):
+        t = timezone.localtime(timezone.now())
+        return queryset.filter(
+            spirit_prophecy_chapter__start_date__year = t.year,
+            spirit_prophecy_chapter__start_date__month = t.month, spirit_prophecy_chapter__start_date__day = t.day,
+        )
